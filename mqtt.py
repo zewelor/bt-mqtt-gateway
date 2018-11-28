8770c625cc291905fb72ae1e056b0161aa037392
unconfigured <8314092+unconfigured@users.noreply.github.com>
2018-11-28 11:53:06 +0100

    Fix mqtt prefix handling on thermostat worker

--- mqtt.py
+++ mqtt.py
@@ -59,6 +59,8 @@ class MqttClient:
     self.mqttc.connect(self.hostname, port=self.port)
 
     for topic, callback in callbacks:
+      if self.topic_prefix is not None:
+        topic = "{}/{}".format(self.topic_prefix, topic)
       self.mqttc.message_callback_add(topic, callback)
       self.mqttc.subscribe(topic)
 
