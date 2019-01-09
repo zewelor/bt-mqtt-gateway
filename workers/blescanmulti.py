d1454e5fb917d118f209ecee497d5f911092dd4c
Elvio Sebastianelli <elvio.sebastianelli@gmail.com>
2019-01-09 16:29:57 +0100

    Worker for ble devices scanning
    Added a worker that scan for ble devices and notify, by mqtt, presence of mac addresses and rssi.
    
    Configuration section example:
    
    ....
    workers:
        blescanmulti:
          args:
            devices:
              nut: xx:xx:xx:xx:xx:xx
              ionic: xx:xx:xx:xx:xx:xx
            topic_prefix: blescan
          update_interval: 60
    ...

---
+++ workers/blescanmulti.py
@@ -0,0 +1,51 @@
+import time
+from interruptingcow import timeout
+from bluepy.btle import Scanner, DefaultDelegate
+from mqtt import MqttMessage
+import mqtt
+from workers.base import BaseWorker
+from logger import _LOGGER
+
+REQUIREMENTS = ['bluepy']
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            _LOGGER.debug("Discovered new device: %s" % dev.addr)
+
+class BlescanmultiWorker(BaseWorker):
+    def searchmac(self, devices, mac):
+        present = False
+        for dev in devices:
+            if dev.addr == mac:
+               present = True
+
+        return present
+
+    def getrssi(self, devices, mac):
+        rssivalue = -999
+        for dev in devices:
+            if dev.addr == mac:
+               rssivalue = dev.rssi
+
+        return rssivalue
+
+    def status_update(self):
+        scanner = Scanner().withDelegate(ScanDelegate())
+        devices = scanner.scan(10.0)
+        ret = []
+
+        for name, mac in self.devices.items():
+          try:
+            if self.searchmac(devices, mac):
+              ret.append(MqttMessage(topic=self.format_topic('presence/rssi/'+mac), payload=str(self.getrssi(devices,mac))))
+              ret.append(MqttMessage(topic=self.format_topic('presence/'+mac), payload="1"))
+            else:
+              ret.append(MqttMessage(topic=self.format_topic('presence/'+mac), payload="0"))
+          except RuntimeError:
+              pass
+
+        return ret
