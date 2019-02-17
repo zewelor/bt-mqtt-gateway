ac746b82a1c6a53fcd4ebfa4cd42231dafaa7de2
Dennis Keitzel <github@pinshot.net>
2019-02-17 20:29:53 +0100

    Do not clear cache for each attribute

--- workers/miflora.py
+++ workers/miflora.py
@@ -11,9 +11,8 @@ class MifloraWorker(BaseWorker):
     from miflora.miflora_poller import MiFloraPoller
     from btlewrap.bluepy import BluepyBackend
 
-
     for name, mac in self.devices.items():
-      self.devices[name] = MiFloraPoller(mac, BluepyBackend, cache_timeout=0)
+      self.devices[name] = MiFloraPoller(mac, BluepyBackend)
 
   def status_update(self):
     ret = []
@@ -22,16 +21,12 @@ class MifloraWorker(BaseWorker):
         ret += self.update_device_state(name, poller)
       except RuntimeError:
         pass
-
     return ret
 
   @timeout(8.0)
-
   def update_device_state(self, name, poller):
-
     ret = []
+    poller.clear_cache()
     for attr in monitoredAttrs:
-
       ret.append(MqttMessage(topic=self.format_topic(name, attr), payload=poller.parameter_value(attr)))
-
     return ret
