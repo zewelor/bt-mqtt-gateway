fda792c51a546c73b655039de8bcf093d100c82f
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2019-06-18 08:18:22 +0200

    add mqtt discovery for miflora

--- workers/miflora.py
+++ workers/miflora.py
@@ -1,6 +1,4 @@
-import logging
-
-from mqtt import MqttMessage
+from mqtt import MqttMessage, MqttConfigMessage
 
 from workers.base import BaseWorker
 import logger
@@ -20,8 +18,46 @@ class MifloraWorker(BaseWorker):
       _LOGGER.debug("Adding %s device '%s' (%s)", repr(self), name, mac)
       self.devices[name] = MiFloraPoller(mac, BluepyBackend)
 
+  def config(self):
+    ret = []
+    for name, poller in self.devices.items():
+      ret += self.config_device(name)
+    return ret
+
+  def config_device(self, name):
+    ret = []
+    device={"identifiers": self.format_topic(name, separator="_"),
+            "manufacturer": "Xiaomi",
+            "model": "MiFlora",
+            "name": self.format_topic(name, separator=" ").title()}
+    for attr in monitoredAttrs:
+      if attr == 'light':
+        payload = {"unique_id": self.format_topic(name, 'illuminance', separator="_"),
+                   "state_topic": self.format_topic(name, attr),
+                   "device_class": 'illuminance',
+                   "device": device}
+      elif attr == 'moisture':
+        payload = {"unique_id": self.format_topic(name, attr, separator="_"),
+                   "state_topic": self.format_topic(name, attr),
+                   "icon": 'mdi:water',
+                   "unit_of_measurement": "%",
+                   "device": device}
+      elif attr == 'conductivity':
+        payload = {"unique_id": self.format_topic(name, attr, separator="_"),
+                   "state_topic": self.format_topic(name, attr),
+                   "icon": 'mdi:leaf',
+                   "unit_of_measurement": "µS/cm",
+                   "device": device}
+      else:
+        payload = {"unique_id": self.format_topic(name, attr, separator="_"),
+                   "state_topic": self.format_topic(name, attr),
+                   "device_class": attr,
+                   "device": device}
+      ret.append(MqttConfigMessage(MqttConfigMessage.SENSOR, self.format_topic(name, attr, separator="_"), payload=payload))
+
+    return ret
+
   def status_update(self):
-    from btlewrap.base import BluetoothBackendException
     _LOGGER.info("Updating %d %s devices", len(self.devices), repr(self))
     ret = []
     for name, poller in self.devices.items():
