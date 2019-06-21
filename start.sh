2f5d6f024cde6c814ea58806ce1a2663828c1b90
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2019-06-21 15:33:39 +0200

    exit with gateway status on debug mode

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
 
- if ! [ -f '/config/config.yaml' ]; then
+if ! [ -f '/config/config.yaml' ]; then
     echo "There is no config.yaml! An example is created."
     cp /application/config.yaml.example /config/config.yaml.example
     exit 1
@@ -10,7 +10,9 @@ cd /application
 if [ "$DEBUG" = 'true' ]; then
     echo "Start in debug mode"
     python3 ./gateway.py -d
+    status=$?
     echo "Gateway died..."
+    exit $status
 else
     echo "Start in normal mode"
     python3 ./gateway.py
