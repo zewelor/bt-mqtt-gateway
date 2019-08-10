c020a37b7636403f4a917098da55733459121bb3
Carles Amigó <fr3nd@fr3nd.net>
2019-08-10 18:11:18 +0200

    Handle TimeoutError exception properly
    When having more than one device, if one was timing out, no device was
    being updated.

--- workers/mithermometer.py
+++ workers/mithermometer.py
@@ -65,6 +65,8 @@ class MithermometerWorker(BaseWorker):
         ret += self.update_device_state(name, data["poller"])
       except BluetoothBackendException as e:
         logger.log_exception(_LOGGER, "Error during update of %s device '%s' (%s): %s", repr(self), name, data["mac"], type(e).__name__, suppress=True)
+      except TimeoutError as e:
+        logger.log_exception(_LOGGER, "Time out during update of %s device '%s' (%s): %s", repr(self), name, data["mac"], type(e).__name__, suppress=True)
     return ret
 
   def update_device_state(self, name, poller):
