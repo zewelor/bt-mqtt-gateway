07a93272b59c8ca4ecbe90a6cd2df5dfdf6131b6
zewelor <zewelor@gmail.com>
2019-10-03 16:30:46 +0200

    Improve linak desk worker

--- workers/linakdesk.py
+++ workers/linakdesk.py
@@ -1,11 +1,14 @@
 from interruptingcow import timeout
 
+import logger
 from exceptions import DeviceTimeoutError
 from mqtt import MqttMessage
 from workers.base import BaseWorker
 
+_LOGGER = logger.get(__name__)
+
 REQUIREMENTS = [
-    "git+https://github.com/zewelor/linak_bt_desk.git@aa9412f98b3044be34c70e89d02721e6813ea731#egg=linak_bt_desk"
+    "git+https://github.com/zewelor/linak_bt_desk.git@aa9412f98b3044be34c70e89d02721e6813ea731#egg=linakdpgbt"
 ]
 
 
@@ -26,6 +29,8 @@ class LinakdeskWorker(BaseWorker):
         ]
 
     def _get_height(self):
+        from bluepy import btle
+
         with timeout(
             self.SCAN_TIMEOUT,
             exception=DeviceTimeoutError(
@@ -34,7 +39,16 @@ class LinakdeskWorker(BaseWorker):
                 )
             ),
         ):
-            self.desk.read_dpg_data()
-            return self.desk.current_height_with_offset.cm
-
-        return -1
+            try:
+                self.desk.read_dpg_data()
+                return self.desk.current_height_with_offset.cm
+            except btle.BTLEException as e:
+                logger.log_exception(
+                    _LOGGER,
+                    "Error during update of linak desk '%s' (%s): %s",
+                    repr(self),
+                    self.mac,
+                    type(e).__name__,
+                    suppress=True,
+                )
+                raise DeviceTimeoutError
