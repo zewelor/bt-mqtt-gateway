a8362535c0227c89f486c5f65c77e3a9d5311056
Dennis Keitzel <github@pinshot.net>
2020-05-31 13:06:30 +0200

    Verify configured requirements

--- gateway.py
+++ gateway.py
@@ -16,6 +16,7 @@ import logging
 import argparse
 import queue
 
+import workers_requirements
 from workers_queue import _WORKERS_QUEUE
 from mqtt import MqttClient
 from workers_manager import WorkersManager
@@ -51,8 +52,6 @@ parser.add_argument("-r", "--requirements", type=str, choices=['all', 'configure
 parsed = parser.parse_args()
 
 if parsed.requirements:
-    import workers_requirements
-
     requirements = []
     if parsed.requirements == 'configured':
         requirements = workers_requirements.configured_workers()
@@ -73,6 +72,8 @@ else:
 logger.suppress_update_failures(parsed.suppress)
 
 _LOGGER.info("Starting")
+
+workers_requirements.verify()
 
 global_topic_prefix = settings["mqtt"].get("topic_prefix")
 
--- workers_requirements.py
+++ workers_requirements.py
@@ -1,5 +1,11 @@
 import importlib
 from pathlib import Path
+import pkg_resources
+import re
+
+import logger
+
+_LOGGER = logger.get(__name__)
 
 
 def configured_workers():
@@ -12,6 +18,39 @@ def configured_workers():
 def all_workers():
     workers = map(lambda x: x.stem, Path('./workers').glob('*.py'))
     return _get_requirements(workers)
+
+
+def verify():
+    requirements = configured_workers()
+    egg = re.compile(r'.+#egg=(.+)$')
+
+    distributions = []
+    for req in requirements:
+        try:
+            pkg_resources.Requirement.parse(req)
+            distributions.append(req)
+        except (pkg_resources.extern.packaging.requirements.InvalidRequirement,
+                pkg_resources.RequirementParseError):
+            match = egg.match(req)
+            if match:
+                distributions.append(match.group(1))
+            else:
+                raise
+
+    errors = []
+    for dist in distributions:
+        try:
+            pkg_resources.require(dist)
+        except pkg_resources.ResolutionError as e:
+            errors.append(e.report())
+
+    if errors:
+        _LOGGER.error('Error: unsatisfied requirements:')
+        for error in errors:
+            _LOGGER.error('  %s', error)
+        _LOGGER.error('You may install those with pip: python -m pip install %s',
+                      ' '.join(requirements))
+        exit(1)
 
 
 def _get_requirements(workers):
