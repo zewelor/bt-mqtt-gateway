e275cca44894d4d2a2c962ff236a14e20505e97b
David Hutchison <dhutchison86+github@googlemail.com>
2021-01-16 00:10:06 +0000

    initial position set support

--- workers/am43.py
+++ workers/am43.py
@@ -1,9 +1,6 @@
-from interruptingcow import timeout
-
 from const import DEFAULT_PER_DEVICE_TIMEOUT
 
 import logger
-from exceptions import DeviceTimeoutError
 from mqtt import MqttMessage
 from workers.base import BaseWorker
 
@@ -25,8 +22,8 @@ class Am43Worker(BaseWorker):
         _LOGGER.info("Updating %d %s devices", len(self.devices), repr(self))
         import Zemismart
         
-        for name, data in self.devices.items():
-            _LOGGER.debug("Updating %s device '%s' (%s)", repr(self), name, data["mac"])
+        for device_name, data in self.devices.items():
+            _LOGGER.debug("Updating %s device '%s' (%s)", repr(self), device_name, data["mac"])
             
             shade = Zemismart.Zemismart(data["mac"], data["pin"], max_connect_time=self.per_device_timeout)
             with shade:
@@ -44,16 +41,44 @@ class Am43Worker(BaseWorker):
 
                         yield [
                             MqttMessage(
-                                topic=self.format_topic(name, "currentPosition"), payload=shade.position
+                                topic=self.format_topic(device_name, "currentPosition"), payload=shade.position
                             ),
                             MqttMessage(
-                                topic=self.format_topic(name, "battery"), payload=shade.battery
+                                topic=self.format_topic(device_name, "battery"), payload=shade.battery
+                            ),
+                            # TODO: How to get this state?
+                            MqttMessage(
+                                topic=self.format_topic(device_name, "positionState"), payload='STOPPED'
                             )
                         ]
                     else:
-                        _LOGGER.debug("Got battery state 0 for '%s' (%s)", name, data["mac"])
+                        _LOGGER.debug("Got battery state 0 for '%s' (%s)", device_name, data["mac"])
 
     def on_command(self, topic, value):
         _LOGGER.info("On command called with %s %s", topic, value)
+        import Zemismart
 
+        topic_without_prefix = topic.replace("{}/".format(self.topic_prefix), "")
+        device_name, action = topic_without_prefix.split("/")
         # TODO: targetPosition
+
+        if device_name in self.devices:
+            data = self.devices[device_name]
+        else:
+            logger.log_exception(_LOGGER, "Ignore command because device %s is unknown", device_name)
+            return []
+
+        value = value.decode("utf-8")
+        if action == "targetPosition":
+            shade = Zemismart.Zemismart(data["mac"], data["pin"], max_connect_time=self.per_device_timeout)
+            with shade:
+                shade.set_position(int(value))
+
+        # TODO: Return updated state messages?
+        # need to:
+        # - get the current position before setting
+        # - update for ascending / descending based on if the target is higher or lower
+        # - loop (with sleeps) until current = target? As it looks like the library will get updates 
+        #   while it is in the "with shade" context
+
+        return []
