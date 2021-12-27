9e6eb271d2eb031695144f6bc864ff90c8edccff
zewelor <zewelor@gmail.com>
2021-12-27 19:18:24 +0100

    Cleanup workflow

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if ! [ -f '/config.yaml' ]; then
-    echo "There is no config.yaml! An example is created."
-    cp /application/config.yaml.example /config.yaml.example
+if ! [ -f '/application/config.yaml' ]; then
+    echo "There is no config.yaml! Check example config: https://github.com/zewelor/bt-mqtt-gateway/blob/master/config.yaml.example"
     exit 1
 fi
 
-cd /application
 if [ "$DEBUG" = 'true' ]; then
     echo "Start in debug mode"
     python3 ./gateway.py -d
